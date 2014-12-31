d487c864b7c747dc6772852ccf35671ee795ef27
Andrew Zures <azures@8thlight.com>
2014-12-30 20:36:50 -0600

    thru 16, skipped 13

--- nintynine/secondTen/secondTen.hs
+++ nintynine/secondTen/secondTen.hs
@@ -23,8 +23,38 @@ encodeModified' (x:xs) (Multiple i v)
 -- #12
 decodeModified :: (Eq a) => [Encoded a] -> [a]
 decodeModified [] = []
+-- decodeModified (x:xs) = (decode' x) : decodeModified xs
 decodeModified (x:xs) = (decode' x) ++ decodeModified xs
 
 decode' :: (Eq a) => Encoded a -> [a]
 decode' (Single v) = [v]
 decode' (Multiple i v) = replicate i v
+
+-- #skipping #13 for now
+
+-- #14
+dupli :: [a] -> [a]
+dupli [] = []
+dupli (x:xs) = x : x : (dupli xs)
+
+-- #15
+repli :: [a] -> Int -> [a]
+repli [] _ = []
+repli (x:xs) n = (replin x n) ++ (repli xs n)
+
+replin :: a -> Int -> [a]
+replin x n  = replin' x n 0
+
+replin' :: a -> Int -> Int -> [a]
+replin' x n inc | n <= inc   = []
+                | otherwise  = x : replin' x n (succ inc)
+
+-- #16
+dropEvery :: [a] -> Int -> [a]
+dropEvery xs n  = dropEvery' xs n 1
+
+dropEvery' :: [a] -> Int -> Int -> [a]
+dropEvery' [] _ _ = []
+dropEvery' (x:xs) n i | (i `mod` n) == 0   = next
+                      | otherwise          = x : next
+                      where next = dropEvery' xs n (succ i)
--- nintynine/secondTen/secondTenSpec.hs
+++ nintynine/secondTen/secondTenSpec.hs
@@ -26,5 +26,22 @@ main = hspec $ do
                      Multiple 2 1,Single 4, Multiple 4 5]
       decodeModified encoded `shouldBe` do [1,1,1,1,2,3,3,1,1,4,5,5,5,5]
 
+    it "#14a duplicates a list" $ do
+      dupli [1,2,3] `shouldBe` [1,1,2,2,3,3]
+
+    it "#14b duplicates a list" $ do
+      dupli "radf" `shouldBe` "rraaddff"
+
+    it "#15a replicates each element in a list n times" $ do
+      repli [1,2,3] 3 `shouldBe` [1,1,1,2,2,2,3,3,3]
+
+    it "#15b replicates each element in a list n times" $ do
+      repli "trew" 3 `shouldBe` "tttrrreeewww"
+
+
+    it "#16 drops every nth element" $ do
+      (dropEvery "abcdefghik" 3) `shouldBe` "abdeghk"
+
+
 
 
