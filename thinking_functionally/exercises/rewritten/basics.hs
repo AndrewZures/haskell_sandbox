b27a074deaa76b9578528bada78de77139245cd6
Andrew Zures <azures@8thlight.com>
2015-08-07 06:46:10 -0500

    rewrites zip and zipWith

--- thinking_functionally/exercises/rewritten/basics.hs
+++ thinking_functionally/exercises/rewritten/basics.hs
@@ -18,3 +18,11 @@ curryFilter f = concat . map (test f)
 
 test :: (a -> Bool) -> a -> [a]
 test f x = if f x then [x] else []
+
+myZip :: [a] -> [b] -> [(a,b)]
+myZip (x:xs) (y:ys) = (x,y):myZip xs ys
+myZip  _     _      = []
+
+myZipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
+myZipWith  f (x:xs) (y:ys) = f x y : myZipWith f xs ys
+myZipWith  _ _      _      = []
--- thinking_functionally/exercises/rewritten/basicsSpec.hs
+++ thinking_functionally/exercises/rewritten/basicsSpec.hs
@@ -18,3 +18,11 @@ main = hspec $ do
     it "rewritten curry filter" $ do
       Basics.curryFilter (1 <) [1,2,3] `shouldBe` [2,3]
 
+    it "rewritten zip" $ do
+      Basics.myZip ["a", "b"] [1,2] `shouldBe` [("a", 1), ("b", 2)]
+
+    it "rewritten zipWith" $ do
+      Basics.myZipWith (+) [4,5] [1,2] `shouldBe` [5,7]
+
+    it "rewritten zip with zipWith" $ do
+      Basics.myZipWith (,) [4,5] [1,2] `shouldBe` [(4,1),(5,2)]
